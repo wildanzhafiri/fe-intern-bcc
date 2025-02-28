df543e327ee6acd4b1d3d20aa4e1ade03ce6d93b
wildan <wildanzhaf@gmail.com>
2025-02-28 20:27:10 +0700

    style(layouts): update Footer with colors and responsiveness

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,46 +1,51 @@
-import React from 'react';
+import logodanlentara from '../assets/logo dan lentara.png';
+import instagramLogo from '../assets/instagram.png';
+import tiktokLogo from '../assets/tiktok.png';
+import linkedinLogo from '../assets/linkedin.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-300 text-xs">
-      <div className="h-8 bg-gray-400"></div>
+    <footer className="bg-tertiary-400 text-base h-full">
+      <div className="h-11 bg-tertiary-300"></div>
 
-      <div className="container mx-auto py-12 px-10 grid grid-cols-4">
-        <div className="flex flex-col items-center">
-          <div className="w-32 h-10 bg-gray-700 mb-2"></div>
-          <p className="text-gray-600">your business slogan</p>
+      <div className="container mx-auto py-16 px-6 flex flex-col md:flex-row justify-center gap-10 md:gap-12 lg:gap-32 text-center md:text-left">
+        <div className="flex flex-col items-center w-full md:w-auto">
+          <div className="flex items-center w-40 lg:w-48 h-auto mx-4 md:mx-0">
+            <img src={logodanlentara} alt="lentara" className="h-12 md:h-16 w-auto object-contain" />
+          </div>
+          <p className="text-white">Illuminating your life</p>
         </div>
 
-        <div>
-          <h3 className="font-bold mb-2">(Nama platform)</h3>
-          <ul className="space-y-1">
-            <li>Tentang Kami</li>
-            <li>Blog</li>
-            <li>Promo</li>
-            <li>Metode Pembayaran</li>
-            <li>Lacak Pesanan</li>
-            <li>Pengembalian Barang</li>
-            <li>Garansi</li>
-            <li>Mulai Jual</li>
+        <div className="w-full md:w-auto">
+          <h3 className="font-bold mb-2 text-white">(Nama platform)</h3>
+          <ul className="space-y-2 text-white">
+            <li className="cursor-pointer hover:text-black">Tentang Kami</li>
+            <li className="cursor-pointer hover:text-black">Blog</li>
+            <li className="cursor-pointer hover:text-black">Promo</li>
+            <li className="cursor-pointer hover:text-black">Metode Pembayaran</li>
+            <li className="cursor-pointer hover:text-black">Lacak Pesanan</li>
+            <li className="cursor-pointer hover:text-black">Pengembalian Barang</li>
+            <li className="cursor-pointer hover:text-black">Garansi</li>
+            <li className="cursor-pointer hover:text-black">Mulai Jual</li>
           </ul>
         </div>
 
-        <div>
-          <h3 className="font-bold mb-2">Layanan</h3>
-          <ul className="space-y-1 text-gray-600">
-            <li>Pusat Bantuan</li>
-            <li>Kontak Media</li>
-            <li>Kebijakan dan Privasi</li>
-            <li>FAQs</li>
+        <div className="w-full md:w-auto">
+          <h3 className="font-bold mb-2 text-white">Layanan</h3>
+          <ul className="space-y-2 text-white">
+            <li className="cursor-pointer hover:text-black">Pusat Bantuan</li>
+            <li className="cursor-pointer hover:text-black">Kontak Media</li>
+            <li className="cursor-pointer hover:text-black">Kebijakan dan Privasi</li>
+            <li className="cursor-pointer hover:text-black">FAQs</li>
           </ul>
         </div>
 
-        <div>
-          <h3 className="font-bold mb-2">Ikuti Kami</h3>
+        <div className="flex flex-col items-center md:items-start w-full md:w-auto">
+          <h3 className="font-bold mb-4 text-white">Ikuti Kami</h3>
           <div className="flex space-x-4">
-            <div className="w-10 h-10 bg-gray-500 rounded-full"></div>
-            <div className="w-10 h-10 bg-gray-500 rounded-full"></div>
-            <div className="w-10 h-10 bg-gray-500 rounded-full"></div>
+            <img src={instagramLogo} className="w-10 h-10 md:w-12 md:h-12 hover:bg-gray-600 cursor-pointer rounded-full"></img>
+            <img src={tiktokLogo} className="w-10 h-10 md:w-12 md:h-12 hover:bg-gray-600 cursor-pointer rounded-full"></img>
+            <img src={linkedinLogo} className="w-10 h-10 md:w-12 md:h-12 hover:bg-gray-600 cursor-pointer rounded-full"></img>
           </div>
         </div>
       </div>
