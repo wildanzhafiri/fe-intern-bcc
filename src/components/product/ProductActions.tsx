5274d215d3085e5e384ff39d6a786122419a5dab
wildan <wildanzhaf@gmail.com>
2025-02-28 20:30:51 +0700

    feat(product): add ProductActions component

---
+++ src/components/product/ProductActions.tsx
@@ -0,0 +1,13 @@
+import Button from '../ui/Button';
+import { MdOutlineShoppingCart } from 'react-icons/md';
+
+const ProductActions: React.FC = () => {
+  return (
+    <div className="flex gap-4">
+      <Button variant={'transparent'} icon={<MdOutlineShoppingCart className="w-7 h-7" />} text={'Keranjang'} />
+      <Button variant={'third'} text="Sewa Sekarang" />
+    </div>
+  );
+};
+
+export default ProductActions;
