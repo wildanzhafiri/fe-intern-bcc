a48323bc4e9c5415496c16c9c3be640bed011aa7
wildan <wildanzhaf@gmail.com>
2025-02-28 21:09:14 +0700

    feat(app): wrap App with AuthProvider and add ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { AuthProvider } from './contexts/AuthProvider';
 import AppRoutes from './routes/AppRoutes';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <AppRoutes />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
