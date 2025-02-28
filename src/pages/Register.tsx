4505810d26c9d37cd73d21f93499182315dddade
wildan <wildanzhaf@gmail.com>
2025-02-28 20:17:02 +0700

    feat(pages): add Register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from '../components/auth/RegisterForm';
+
+const Register = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
