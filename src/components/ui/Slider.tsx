05300aa28e9b95f274135070ab55e32e927cf192
wildan <wildanzhaf@gmail.com>
2025-03-07 22:11:59 +0700

    refactor(slider): replace external image URLs with local assets

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 
 const images = [
-  ['https://www.incharge.org/wp-content/uploads/2019/08/lower-rent.jpg', 'https://files.theinteriorsaddict.com/uploads/2014/07/Royal-wolf-container-room-studio.jpg'],
-  ['https://i.pinimg.com/736x/ae/c2/0d/aec20d200a5a4e907ba9553f0e7aef53.jpg', 'https://i.pinimg.com/736x/f1/6f/2a/f16f2a11fd92685f77eaa1ed86d9626b.jpg'],
-  ['https://i.pinimg.com/736x/6c/a8/02/6ca80277c4de3949cbf863858c08873c.jpg', 'https://i.pinimg.com/736x/95/89/c2/9589c200968b7bb9e4e677feaa117dc5.jpg'],
-  ['https://i.pinimg.com/736x/f4/8b/c4/f48bc44bdf3847d4e91cae3525b94873.jpg', 'https://i.pinimg.com/736x/af/76/90/af7690bd5c902cd26e9cc87601ca1278.jpg'],
+  ['/src/assets/banner1.png', '/src/assets/banner2.png'],
+  ['/src/assets/banner1.png', '/src/assets/banner2.png'],
+  ['/src/assets/banner1.png', '/src/assets/banner2.png'],
+  ['/src/assets/banner1.png', '/src/assets/banner2.png'],
 ];
 
 const Slider: React.FC = () => {
@@ -34,11 +34,11 @@ const Slider: React.FC = () => {
                         flex-col sm:flex-row"
             >
               <div className="w-full h-[250px] sm:w-1/2 sm:h-[300px] lg:h-[400px]">
-                <img src={pair[0]} alt={`Slide ${index + 1} - Image 1`} className="w-full h-full rounded-xl object-cover" />
+                <img src={pair[0]} alt={`Slide ${index + 1} - Image 1`} className="w-full h-full rounded-xl object-contain" />
               </div>
 
               <div className="w-full h-[250px] sm:w-1/2 sm:h-[300px] lg:h-[400px]">
-                <img src={pair[1]} alt={`Slide ${index + 1} - Image 2`} className="w-full h-full rounded-xl object-cover" />
+                <img src={pair[1]} alt={`Slide ${index + 1} - Image 2`} className="w-full h-full rounded-xl object-contain" />
               </div>
             </div>
           ))}
