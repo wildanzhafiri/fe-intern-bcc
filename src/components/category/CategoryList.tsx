ffab4c61b8e25ed235d69ec8065ade3327f68ee5
wildan <wildanzhaf@gmail.com>
2025-03-07 21:26:50 +0700

    feat(category): pass value prop in CategoryList

--- src/components/category/CategoryList.tsx
+++ src/components/category/CategoryList.tsx
@@ -6,7 +6,7 @@ const CategoryList: React.FC = () => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 mx-auto w-fit">
       {categories.map((category) => (
-        <CategoryItem key={category.id} name={category.name} icon={category.icon} />
+        <CategoryItem key={category.id} name={category.name} icon={category.icon} value={category.value} />
       ))}
     </div>
   );
