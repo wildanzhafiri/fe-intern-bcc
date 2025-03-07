dee50c27c0b3cad8077e39717be5ba367b9e5358
wildan <wildanzhaf@gmail.com>
2025-03-07 21:33:24 +0700

    fix(layout): adjust main content width

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow mx-auto px-4 md:px-10 pt-24 md:pt-32">{children}</main>
+      <main className="flex-grow w-full mx-auto px-4 md:px-10 pt-24 md:pt-32">{children}</main>
       <Footer />
     </div>
   );
