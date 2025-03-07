a9a40349c7e017d316bc053d6f66c70768cec007
wildan <wildanzhaf@gmail.com>
2025-03-07 21:41:48 +0700

    fix(ui): update button variant for ProductActions

--- src/components/product/ProductActions.tsx
+++ src/components/product/ProductActions.tsx
@@ -4,7 +4,7 @@ import { MdOutlineShoppingCart } from 'react-icons/md';
 const ProductActions: React.FC = () => {
   return (
     <div className="flex gap-4">
-      <Button variant={'transparent'} icon={<MdOutlineShoppingCart className="w-7 h-7" />} text={'Keranjang'} />
+      <Button variant={'transparent2'} icon={<MdOutlineShoppingCart className="w-7 h-7" />} text={'Keranjang'} />
       <Button variant={'third'} text="Sewa Sekarang" />
     </div>
   );
