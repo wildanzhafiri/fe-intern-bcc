4b213bb59e8b9db677da2a06d9136b1662a3a63d
wildan <wildanzhaf@gmail.com>
2025-03-07 21:28:58 +0700

    feat(ui): improve Button component with new variants

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,7 +2,7 @@ interface ButtonProps {
   text?: string;
   onClick?: () => void;
   type?: 'button' | 'submit' | 'reset';
-  variant?: 'primary' | 'secondary' | 'third' | 'danger' | 'success' | 'login' | 'arrowButton' | 'none' | 'transparent';
+  variant?: 'primary' | 'secondary' | 'third' | 'daftar' | 'success' | 'login' | 'arrowButton' | 'none' | 'transparent' | 'transparent2';
   disabled?: boolean;
   icon?: React.ReactNode;
   full?: boolean;
@@ -12,13 +12,14 @@ const Button: React.FC<ButtonProps> = ({ text, onClick, type = 'button', variant
   const buttonStyles = {
     primary: 'bg-blue-500 rounded-lg px-3 md:px-6 py-2 hover:bg-blue-700 text-white',
     secondary: 'bg-gray-500 rounded-lg px-3 md:px-6 py-2 hover:bg-gray-700 text-white',
-    third: 'bg-tertiary-400 rounded-lg px-3 md:px-6 py-2 hover:bg-tertiary-500 text-white',
-    danger: 'bg-red-500 rounded-lg px-3 md:px-6 py-2 hover:bg-red-700 text-white',
+    third: 'bg-[#5e5e9b] rounded-lg px-3 md:px-6 py-2 hover:bg-primary-400 text-white',
+    daftar: 'bg-secondary-800 rounded-lg px-3 md:px-6 py-2 hover:bg-secondary-600 text-[#383838]',
     success: 'bg-green-500 rounded-lg px-3 md:px-6 py-2 hover:bg-green-700 text-white',
     login: 'bg-green-500 rounded-lg px-3 md:px-6 py-2 hover:bg-green-700 text-white',
     arrowButton: 'px-3 py-3 bg-[#A3A3A3] rounded-full hover:scale-105 transition-transform duration-300',
     none: '',
-    transparent: 'border border-tertiary-400 text-tertiary-400 rounded-lg px-3 md:px-6 py-2 hover:bg-gray-300 transition-transform duration-300',
+    transparent: 'border border-white text-white rounded-lg px-3 md:px-6 py-2 hover:bg-primary-600 transition-transform duration-300',
+    transparent2: 'border border-primary-400 text-primary-400 rounded-lg px-3 md:px-6 py-2 hover:bg-primary-600 transition-transform duration-300',
   };
 
   return (
