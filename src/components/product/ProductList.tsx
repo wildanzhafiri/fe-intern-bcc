5fdedbed643e6d0c695fd08900a6cf63d7fcb36a
wildan <wildanzhaf@gmail.com>
2025-03-07 21:46:03 +0700

    feat(product): fetch product list based on searchQuery and category

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,27 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchDummyProducts } from '../../services/productService';
 import ProductCard from './ProductCard';
 import { useNavigate } from 'react-router-dom';
+import { useProducts } from '../../hooks/useProduct';
 
-const ProductList: React.FC = () => {
-  const { data: products, isLoading } = useQuery({
-    queryKey: ['products'],
-    queryFn: fetchDummyProducts,
-  });
+interface ProductListProps {
+  searchQuery?: string;
+  category?: string;
+}
+
+const ProductList: React.FC<ProductListProps> = ({ searchQuery = '', category = '' }) => {
+  const { data: products, isLoading, error } = useProducts(searchQuery, category);
 
   const navigate = useNavigate();
 
+  if (isLoading) return <p className="text-center">Loading...</p>;
+  if (error) return <p className="text-center text-red-500">Failed to fetch products</p>;
+
   return (
-    <div className="my-10">
-      {isLoading ? (
-        <p className="text-center">Loading...</p>
-      ) : (
-        <div className="grid grid-cols-2 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5  space-y-6 gap-x-3 md:space-y-6 md:gap-x-6 mx-auto max-w-screen-xl">
-          {products?.map((product) => (
-            <ProductCard onClick={() => navigate('/productDetail')} key={product.id} product={product} />
-          ))}
-        </div>
-      )}
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 my-10 max-w-screen-xl">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 md:gap-6">
+        {products?.map((product) => (
+          <ProductCard key={product.id} product={product} onClick={() => navigate(`/productDetail/${product.id}`)} />
+        ))}
+      </div>
     </div>
   );
 };
