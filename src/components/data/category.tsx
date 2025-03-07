d529207aa6eeae8967ca905a34fd6afe726bebf6
wildan <wildanzhaf@gmail.com>
2025-03-07 21:21:53 +0700

    feat(data): add value field to category data

--- src/components/data/category.tsx
+++ src/components/data/category.tsx
@@ -11,25 +11,30 @@ export const categories: Category[] = [
     id: 1,
     name: ProductCategory.ELECTRONICS,
     icon: <FaComputer />,
+    value: 'electronics',
   },
   {
     id: 2,
     name: ProductCategory.TOOLS,
     icon: <GiFamilyHouse />,
+    value: 'tools',
   },
   {
     id: 3,
     name: ProductCategory.HOBBY,
     icon: <PiBeachBallFill />,
+    value: 'hobby',
   },
   {
     id: 4,
     name: ProductCategory.EVENT,
     icon: <LuCalendarClock />,
+    value: 'events',
   },
   {
     id: 5,
     name: ProductCategory.HOLIDAY,
     icon: <SlPlane />,
+    value: 'holiday',
   },
 ];
