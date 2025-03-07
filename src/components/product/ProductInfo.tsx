c152ab6c790a77712b9cd9c523674abbae70f257
wildan <wildanzhaf@gmail.com>
2025-03-07 21:45:13 +0700

    feat(api): integrate ProductInfo with backend

--- src/components/product/ProductInfo.tsx
+++ src/components/product/ProductInfo.tsx
@@ -1,19 +1,25 @@
+// components/product/ProductInfo.tsx
 import { FaStar } from 'react-icons/fa';
 import { PiLineVerticalLight, PiPackage } from 'react-icons/pi';
 import { HiOutlineDotsVertical } from 'react-icons/hi';
+import { Product } from '../../types/product';
 
-const ProductInfo: React.FC = () => {
+interface ProductInfoProps {
+  product: Product;
+}
+
+const ProductInfo: React.FC<ProductInfoProps> = ({ product }) => {
   return (
     <div>
       <div className="flex justify-between">
-        <h1 className="text-xl font-bold">Nama Produk</h1>
+        <h1 className="text-xl font-bold">{product.title}</h1>
         <div className="px-2">
           <HiOutlineDotsVertical className="w-5 h-5 cursor-pointer" />
         </div>
       </div>
       <div className="flex items-center text-[10px] md:text-sm text-gray-600 mt-2">
-        <PiPackage className="mr-1 w-4 h-4 md:w-5 md:h-5" /> 18 Peminjam <PiLineVerticalLight className="w-5 h-5" />
-        <FaStar className="mx-1 w-3 h-3 md:w-4 md:h-4" /> 4.9/5
+        <PiPackage className="mr-1 w-4 h-4 md:w-5 md:h-5" /> {product.rent_count} Peminjam <PiLineVerticalLight className="w-5 h-5" />
+        <FaStar className="mx-1 w-3 h-3 md:w-4 md:h-4" /> {product.rating}/5
       </div>
     </div>
   );
