e39f195eb979889d8923e2a4cedbc65e3892d354
wildan <wildanzhaf@gmail.com>
2025-03-14 14:43:14 +0700

    style: update button colors and variant

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,7 +2,7 @@ interface ButtonProps {
   text?: string;
   onClick?: () => void;
   type?: 'button' | 'submit' | 'reset';
-  variant?: 'primary' | 'secondary' | 'third' | 'daftar' | 'success' | 'login' | 'arrowButton' | 'none' | 'transparent' | 'transparent2';
+  variant?: 'primary' | 'secondary' | 'third' | 'daftar' | 'addProduct' | 'login' | 'arrowButton' | 'none' | 'transparent' | 'transparent2';
   disabled?: boolean;
   icon?: React.ReactNode;
   full?: boolean;
@@ -10,16 +10,16 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ text, onClick, type = 'button', variant = 'primary', disabled = false, icon, full = false }) => {
   const buttonStyles = {
-    primary: 'bg-blue-500 rounded-lg px-3 md:px-6 py-2 hover:bg-blue-700 text-white',
+    primary: 'bg-primary-400 rounded-lg px-3 md:px-6 py-2 hover:bg-primary-600 text-white',
     secondary: 'bg-gray-500 rounded-lg px-3 md:px-6 py-2 hover:bg-gray-700 text-white',
-    third: 'bg-[#5e5e9b] rounded-lg px-3 md:px-6 py-2 hover:bg-primary-400 text-white',
+    third: 'bg-primary-400 rounded-lg px-3 md:px-6 py-2 hover:bg-primary-600 text-white',
     daftar: 'bg-secondary-800 rounded-lg px-3 md:px-6 py-2 hover:bg-secondary-600 text-[#383838]',
-    success: 'bg-green-500 rounded-lg px-3 md:px-6 py-2 hover:bg-green-700 text-white',
+    addProduct: 'bg-primary-400 rounded-full px-3 md:px-6 py-3 hover:bg-primary-600 text-white whitespace-nowrap h-full item-center justify-center',
     login: 'bg-green-500 rounded-lg px-3 md:px-6 py-2 hover:bg-green-700 text-white',
     arrowButton: 'px-3 py-3 bg-[#A3A3A3] rounded-full hover:scale-105 transition-transform duration-300',
     none: '',
     transparent: 'border border-white text-white rounded-lg px-3 md:px-6 py-2 hover:bg-primary-600 transition-transform duration-300',
-    transparent2: 'border border-primary-400 text-primary-400 rounded-lg px-3 md:px-6 py-2 hover:bg-primary-600 transition-transform duration-300',
+    transparent2: 'border border-primary-400 text-primary-400 rounded-lg px-3 md:px-6 py-2 hover:bg-primary-400 hover:text-white transition-transform duration-300',
   };
 
   return (
