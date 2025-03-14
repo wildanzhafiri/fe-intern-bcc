92dbc6bd3f08e1abc431310f0556db117c4a8efb
wildan <wildanzhaf@gmail.com>
2025-03-14 14:04:44 +0700

    feat: add owner login page

---
+++ src/pages/OwnerLogin.tsx
@@ -0,0 +1,11 @@
+import OwnerLoginForm from '../components/auth/OwnerLoginForm';
+
+const OwnerLogin = () => {
+  return (
+    <div>
+      <OwnerLoginForm />
+    </div>
+  );
+};
+
+export default OwnerLogin;
