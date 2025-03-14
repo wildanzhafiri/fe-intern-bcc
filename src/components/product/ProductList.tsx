7a945c9f4ddc2851819dae16ad3b77c82ec4dc03
wildan <wildanzhaf@gmail.com>
2025-03-14 21:41:50 +0700

    style: improve responsiveness

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -22,8 +22,8 @@ const ProductList: React.FC<ProductListProps> = ({ searchQuery = '', category = 
   }
 
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 my-10 max-w-screen-xl">
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 md:gap-6">
+    <div className="container mx-auto px-4 lg:px-8 my-10 max-w-screen-xl">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 md:gap-6">
         {products?.map((product) => (
           <ProductCard key={product.id} product={product} onClick={() => navigate(`/productDetail/${product.id}`)} />
         ))}
