0507842a579b8e6188056d48aa706fe2c6842f94
wildan <wildanzhaf@gmail.com>
2025-03-14 14:23:22 +0700

    style: make layout responsive

--- src/components/payment/OrderItem.tsx
+++ src/components/payment/OrderItem.tsx
@@ -11,31 +11,32 @@ interface OrderItemProps {
 
 const OrderItem: React.FC<OrderItemProps> = ({ name, price, quantity, duration, imageUrl }) => {
   return (
-    <div className="flex max-w-6xl items-center p-4 ">
-      <div className="flex items-center gap-4 w-[30%]">
-        <img src={imageUrl} alt={name} className="w-40 h-40 rounded-lg object-cover" />
-        <h3 className="text-lg font-semibold line-clamp-2">{name}</h3>
+    <div className="flex flex-col md:flex-row max-w-6xl items-start md:items-center p-4 space-y-4 md:space-y-0">
+      <div className="flex flex-col sm:flex-row items-start sm:items-center gap-4 w-full md:w-[30%]">
+        <img src={imageUrl} alt={name} className=" w-24 md:w-28 lg:w-40 h-24 md:h-28 lg:h-40 rounded-lg object-cover" />
+
+        <h3 className="text-lg font-semibold mt-2 sm:mt-0 line-clamp-2 md:line-clamp-none lg:line-clamp-2">{name}</h3>
       </div>
 
-      <div className="flex justify-evenly w-[70%]">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:flex md:justify-evenly w-full md:w-[70%]">
         <div className="flex flex-col">
           <p className="text-sm text-secondary-300">Harga Sewa:</p>
           <p className="text-[#00378b]">Rp{formatPrice(price)}/hari</p>
         </div>
 
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
           <p className="text-sm text-secondary-300">Jumlah:</p>
           <p>{quantity}</p>
         </div>
 
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
           <p className="text-sm text-secondary-300">Durasi Sewa:</p>
           <p>{duration} hari</p>
         </div>
 
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
           <p className="text-sm text-secondary-300">Subtotal:</p>
-          <p className="text-[#00378b] font-bold">Rp{formatPrice(price * duration)}</p>
+          <p className="text-[#00378b] font-bold">Rp{formatPrice(price * quantity * duration)}</p>
         </div>
       </div>
     </div>
