db8fe0668835949dfbde9e5ff6c6b5e7cb36cefc
wildan <wildanzhaf@gmail.com>
2025-03-14 14:34:50 +0700

    feat: update ProductCard with new pricing and origin data

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -11,17 +11,17 @@ interface ProductProps {
 const ProductCard: React.FC<ProductProps> = ({ product, onClick }) => {
   return (
     <div className="border border-gray-300 bg-white cursor-pointer hover:scale-105 transition-transform duration-300 rounded-2xl shadow-md w-full h-[320px] md:h-[380px] flex flex-col" onClick={onClick}>
-      <img src={product.photo_url} alt={`image of ${product.title}`} className="w-full h-44 md:h-56 object-cover rounded-t-2xl mb-2" />
+      <img src={product.photo_url} alt={`image of ${product.title}`} className="w-full h-44 md:h-56 object-contain rounded-t-2xl mb-2" />
       <div className="p-3 flex flex-col flex-grow justify-between">
         <div>
           <h3 className="font-semibold text-sm md:text-base truncate">{product.title}</h3>
-          <p className="font-bold text-primary-400 text-sm md:text-lg">Rp{formatPrice(product.price)}/hari</p>
+          <p className="font-bold text-primary-400 text-sm md:text-lg">Rp{formatPrice(product.price_1)}/hari</p>
         </div>
         <div>
-          <p className="text-xs md:text-base truncate">Kecamatan, Nama Jalan</p>
+          <p className="text-xs md:text-base truncate">{product.origin}</p>
           <div className="flex items-center text-[9px] md:text-[12px] text-gray-600">
             <PiPackage className="mr-1 w-4 h-4 md:w-5 md:h-5 text-primary-400" /> {product.rent_count} Peminjam <PiLineVerticalLight className="w-7 h-7" />
-            <FaStar className="mx-1 w-3 h-3 md:w-4 md:h-4 text-primary-400" /> {product.rating}/5
+            <FaStar className="mx-1 w-2 h-2 md:w-4 md:h-4 text-primary-400" /> {product.rating}/5
           </div>
         </div>
       </div>
