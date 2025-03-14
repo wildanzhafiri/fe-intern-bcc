ae35c14b5bac830cae658344d886beab8d43db3b
wildan <wildanzhaf@gmail.com>
2025-03-14 14:05:02 +0700

    feat: add owner register page

---
+++ src/pages/OwnerRegister.tsx
@@ -0,0 +1,11 @@
+import OwnerRegisterForm from '../components/auth/OwnerRegisterForm';
+
+const OwnerRegister = () => {
+  return (
+    <div>
+      <OwnerRegisterForm />
+    </div>
+  );
+};
+
+export default OwnerRegister;
