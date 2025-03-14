d52231794bcf69da739e63be8d0cd11d839e0b08
wildan <wildanzhaf@gmail.com>
2025-03-14 23:37:41 +0700

    feat: fix cartSumary data

--- src/components/cart/CartSumary.tsx
+++ src/components/cart/CartSumary.tsx
@@ -9,7 +9,7 @@ const Summary = () => {
 
   return (
     <div className="border border-secondary-500 rounded-xl bg-white w-full mx-auto">
-      <div className="p-4 md:p-6">
+      <div className="p-4 md:p-8">
         <h2 className="font-semibold text-lg">Ringkasan Biaya</h2>
 
         <div className="mt-4 space-y-2">
@@ -19,26 +19,14 @@ const Summary = () => {
             <p className="text-red-500">Gagal memuat ringkasan</p>
           ) : (
             <>
-              <div className="flex justify-between">
-                <span>Jumlah Produk</span>
-                <span className="font-medium">{summary?.product_count} Item</span>
-              </div>
-              <div className="flex justify-between">
-                <span>Biaya Pengiriman</span>
-                <span className="font-medium">Rp{formatPrice(summary?.delivery_cost || 0)}</span>
-              </div>
-              <div className="flex justify-between">
-                <span>Biaya Layanan</span>
-                <span className="font-medium">Rp{formatPrice(summary?.service_cost || 0)}</span>
-              </div>
-              <div className="flex justify-between">
-                <span>Deposit ({summary?.deposite_percentage}%)</span>
-                <span className="font-medium">Rp{formatPrice(summary?.deposite_amount || 0)}</span>
-              </div>
-              <div className="flex justify-between">
-                <span>Voucher</span>
-                <span className="font-medium">- Rp{formatPrice(summary?.voucher || 0)}</span>
-              </div>
+              {summary?.payload.map((item, index) => (
+                <div key={index} className="flex justify-between">
+                  <span>{item.name || `Produk ${index + 1}`}</span>
+                  <span className="font-medium">
+                    {item.count} x Rp{formatPrice(item.price)}
+                  </span>
+                </div>
+              ))}
             </>
           )}
         </div>
@@ -46,7 +34,7 @@ const Summary = () => {
 
       <div className="border-t p-4 md:p-6">
         <div className="w-full relative">
-          <select className="w-full appearance-none border rounded p-2 text-secondary-500">
+          <select className="w-full appearance-none border rounded-lg p-2 text-secondary-500">
             <option>Pilih Voucher</option>
           </select>
           <IoMdArrowDropdown className="absolute right-4 top-3 w-5 h-5 text-secondary-500" />
@@ -54,7 +42,7 @@ const Summary = () => {
 
         <div className="flex px-1 justify-between font-bold text-md md:text-lg my-3">
           <span>Total Harga</span>
-          <span>Rp{formatPrice(summary?.total_price || 0)}</span>
+          <span>Rp{formatPrice(summary?.totalPrice ?? 0)}</span>
         </div>
       </div>
 
