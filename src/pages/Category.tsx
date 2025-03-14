44ede9f02c63fe33419ff42d63b8fd283b5741da
wildan <wildanzhaf@gmail.com>
2025-03-14 14:51:13 +0700

    feat: remove queryClient and add new handle for new filter

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,18 +1,28 @@
 import { IoMdArrowDropdown } from 'react-icons/io';
 import ProductList from '../components/product/ProductList';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useSearchParams } from 'react-router-dom';
 
-const queryClient = new QueryClient();
+import { useSearchParams } from 'react-router-dom';
 
 const Category = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const searchQuery = searchParams.get('search') || '';
   const category = searchParams.get('category') || '';
+  const rating = searchParams.get('rating') || '';
+  const priceSort = searchParams.get('price') || '';
 
   const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedCategory = e.target.value;
     searchParams.set('category', selectedCategory);
+    setSearchParams(searchParams);
+  };
+
+  const handleRatingChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    searchParams.set('rating', e.target.value);
+    setSearchParams(searchParams);
+  };
+
+  const handlePriceSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    searchParams.set('price', e.target.value);
     setSearchParams(searchParams);
   };
 
@@ -22,8 +32,8 @@ const Category = () => {
         <div className="w-full md:max-w-7xl md:px-6 lg:px-8 flex flex-wrap md:flex-nowrap justify-center gap-4 md:gap-6 lg:gap-8 ">
           <div className="relative w-full md:flex-1">
             <select
-              className="w-full border border-gray-400 bg-white py-2 px-4 pr-10 rounded-lg shadow-sm hover:bg-gray-200 transition duration-300 appearance-none 
-            focus:border-primary-400 focus:ring-1 focus:ring-primary-400 focus:outline-none focus:bg-primary-800 "
+              className="w-full border border-gray-400 bg-white py-2 px-4 pr-10 rounded-lg shadow-sm  transition duration-300 appearance-none 
+            focus:border-primary-400 focus:outline-none  "
               value={category}
               onChange={handleCategoryChange}
             >
@@ -39,29 +49,38 @@ const Category = () => {
 
           <div className="relative w-full md:flex-1">
             <select
-              className="w-full border border-gray-400 bg-white py-2 px-4 pr-10 rounded-lg shadow-sm hover:bg-gray-200 transition duration-300 appearance-none 
-            focus:border-primary-400 focus:ring-1 focus:ring-primary-400 focus:outline-none focus:bg-primary-800"
+              className="w-full border border-gray-400 bg-white py-2 px-4 pr-10 rounded-lg shadow-sm  transition duration-300 appearance-none 
+              focus:border-primary-400 focus:outline-none "
+              value={rating}
+              onChange={handleRatingChange}
             >
-              <option>Penilaian</option>
+              <option value="">Penilaian</option>
+              <option value="5">★★★★★ </option>
+              <option value="4">★★★★☆ </option>
+              <option value="3">★★★☆☆ </option>
+              <option value="2">★★☆☆☆ </option>
+              <option value="1">★☆☆☆☆ </option>
             </select>
             <IoMdArrowDropdown className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600 w-5 h-5 pointer-events-none" />
           </div>
 
           <div className="relative w-full md:flex-1">
             <select
-              className="w-full border border-gray-400 bg-white py-2 px-4 pr-10 rounded-lg shadow-sm hover:bg-gray-200 transition duration-300 appearance-none 
-            focus:border-primary-400 focus:ring-1 focus:ring-primary-400 focus:outline-none focus:bg-primary-800"
+              className="w-full border border-gray-400 bg-white py-2 px-4 pr-10 rounded-lg shadow-sm  transition duration-300 appearance-none 
+              focus:border-primary-400 focus:outline-none "
+              value={priceSort}
+              onChange={handlePriceSortChange}
             >
-              <option>Harga</option>
+              <option value="">Harga</option>
+              <option value="highest">Tertinggi</option>
+              <option value="lowest">Terendah</option>
             </select>
             <IoMdArrowDropdown className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600 w-5 h-5 pointer-events-none" />
           </div>
         </div>
       </div>
 
-      <QueryClientProvider client={queryClient}>
-        <ProductList searchQuery={searchQuery} category={category} />
-      </QueryClientProvider>
+      <ProductList searchQuery={searchQuery} category={category} rating={rating} price={priceSort} />
     </div>
   );
 };
