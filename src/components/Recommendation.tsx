0ba7050f644d262bc9a967107a2850b78285e255
wildan <wildanzhaf@gmail.com>
2025-03-14 14:12:23 +0700

    chore: remove QueryClientProvider usage

--- src/components/Recommendation.tsx
+++ src/components/Recommendation.tsx
@@ -1,15 +1,11 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ProductList from './product/ProductList';
-
-const queryClient = new QueryClient();
 
 const Recommendation = () => {
   return (
     <div className="mt-10">
       <h2 className="text-center my-7 font-bold text-2xl">Rekomendasi untuk Kamu</h2>
-      <QueryClientProvider client={queryClient}>
-        <ProductList />
-      </QueryClientProvider>
+
+      <ProductList />
     </div>
   );
 };
