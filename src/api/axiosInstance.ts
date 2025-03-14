22117ce7a7216d9657f8faa5ddf8999122d4a2e9
wildan <wildanzhaf@gmail.com>
2025-03-14 15:43:57 +0700

    fix: remove port

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://lentara-be.syafahadyan.com:8080/api/v1',
+  baseURL: 'https://lentara-be.syafahadyan.com/api/v1',
 });
 
 export default axiosInstance;
