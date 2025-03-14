26264b815a78bc0a1a95b94deb1243787624a650
wildan <wildanzhaf@gmail.com>
2025-03-14 14:15:26 +0700

    feat: add QueryClientProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { AuthProvider } from './contexts/AuthProvider';
 import AppRoutes from './routes/AppRoutes';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './utils/ScrollToTop';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <AppRoutes />
-      </BrowserRouter>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <ScrollToTop />
+          <AppRoutes />
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
 
