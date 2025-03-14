d1fc1cffe9b8c8a2b77d5c5dc6b19f00a82d5787
wildan <wildanzhaf@gmail.com>
2025-03-14 23:41:06 +0700

    chore: fix ui

--- src/components/dashboard/DashboardCard.tsx
+++ src/components/dashboard/DashboardCard.tsx
@@ -7,7 +7,7 @@ interface DashboardCardProps {
 
 const DashboardCard: React.FC<DashboardCardProps> = ({ title, value }) => {
   return (
-    <div className="bg-white border border-secondary-500 rounded-xl px-6 py-8 flex flex-col justify-between h-full">
+    <div className="bg-white border border-secondary-500 rounded-xl px-6 py-6 md:py-10 flex flex-col justify-between h-full">
       <div className="flex items-center space-x-2">
         <h3 className="text-gray-500 text-sm font-medium">{title}</h3>
         <SlArrowRight className="w-3 h-3 text-gray-500" />
