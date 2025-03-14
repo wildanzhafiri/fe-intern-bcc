4a8afab701d9e25c224c4f420b82d3ca2bc5b4b8
wildan <wildanzhaf@gmail.com>
2025-03-14 14:40:44 +0700

    integrate StoreInfo with backend API

--- src/components/product/StoreInfo.tsx
+++ src/components/product/StoreInfo.tsx
@@ -1,29 +1,33 @@
-import { LuStore } from 'react-icons/lu';
-import { TbMessage } from 'react-icons/tb';
-import { MdOutlineReport } from 'react-icons/md';
+import { Icon } from '@iconify/react';
 import { GoStarFill } from 'react-icons/go';
 import { CiHeart } from 'react-icons/ci';
 import Button from '../ui/Button';
 import avatarToko from '../../assets/avatarToko.png';
+import { Owner } from '../../types/owner';
 
-const StoreInfo: React.FC = () => {
+interface StoreInfoProps {
+  seller: Owner;
+}
+
+const StoreInfo: React.FC<StoreInfoProps> = ({ seller }) => {
   return (
     <div className="border-t border-b py-6 my-6 flex flex-col md:flex-row items-center gap-10">
       <div className="flex items-center gap-4">
         <img src={avatarToko} className="w-16 h-16 rounded-full"></img>
         <div>
-          <h3 className="text-lg font-semibold">Nama Toko</h3>
+          <h3 className="text-lg font-semibold">{seller?.store_name}</h3>
           <p className="text-gray-600 flex items-center">
-            <GoStarFill className=" mr-1" /> 4.9/5 (10 penyewa)
+            <GoStarFill className="mr-1 text-primary-400" />
+            4.9/5 (10 penyewa)
           </p>
         </div>
       </div>
 
       <div className="flex flex-wrap gap-3">
-        <Button icon={<TbMessage />} variant={'transparent2'} text={'Chat'} />
-        <Button icon={<LuStore />} variant={'transparent2'} text={'Toko'} />
-        <Button icon={<MdOutlineReport className="w-5 h-7" />} variant={'transparent2'} text={'Laporkan'} />
-        <Button icon={<CiHeart className="w-7 h-7" />} variant={'none'} />
+        <Button icon={<Icon icon="tabler:message" className=" w-4 h-4 md:w-6 md:h-6" />} variant={'transparent2'} text={'Chat'} />
+        <Button icon={<Icon icon="tdesign:shop-4" className=" w-4 h-4 md:w-6 md:h-6" />} variant={'transparent2'} text={'Toko'} />
+        <Button icon={<Icon icon="material-symbols:report-outline-rounded" className=" w-4 h-4 md:w-6 md:h-6" />} variant={'transparent2'} text={'Laporkan'} />
+        <Button icon={<CiHeart className="w-8 h-8" />} variant={'none'} />
       </div>
     </div>
   );
