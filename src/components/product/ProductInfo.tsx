3aca3e62ca13bdb4b70ddfe3eb05552cdf669748
wildan <wildanzhaf@gmail.com>
2025-03-14 14:36:40 +0700

    style: update text color

--- src/components/product/ProductInfo.tsx
+++ src/components/product/ProductInfo.tsx
@@ -1,4 +1,3 @@
-// components/product/ProductInfo.tsx
 import { FaStar } from 'react-icons/fa';
 import { PiLineVerticalLight, PiPackage } from 'react-icons/pi';
 import { HiOutlineDotsVertical } from 'react-icons/hi';
@@ -19,7 +18,7 @@ const ProductInfo: React.FC<ProductInfoProps> = ({ product }) => {
       </div>
       <div className="flex items-center text-[10px] md:text-sm text-gray-600 mt-2">
         <PiPackage className="mr-1 w-4 h-4 md:w-5 md:h-5" /> {product.rent_count} Peminjam <PiLineVerticalLight className="w-5 h-5" />
-        <FaStar className="mx-1 w-3 h-3 md:w-4 md:h-4" /> {product.rating}/5
+        <FaStar className="mx-1 w-3 h-3 md:w-4 md:h-4 text-primary-400" /> {product.rating}/5
       </div>
     </div>
   );
